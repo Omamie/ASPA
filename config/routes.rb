04fd0b0750c56f675764c70560d0a93c45142b96
Omamerhi Etefia <omamerhie@gmail.com>
2019-12-03 16:36:13 +0100

    Beuatified the HTML on Devise Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
-  resources :treatments, only: [:new, :create, :destroy, :edit, :update]
-
-  devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :treatments
+devise_for :users
+root to: 'pages#home'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
