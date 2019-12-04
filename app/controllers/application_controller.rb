c4101b2c73e9f4f1c2966ac89c6092934d1b0073
Omamerhi Etefia <omamerhie@gmail.com>
2019-12-04 12:09:35 +0100

    fixed sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-
+   before_action :configure_permitted_parameters, if: :devise_controller?
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # def user_not_authorized
@@ -13,9 +13,13 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
   private
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
 end
